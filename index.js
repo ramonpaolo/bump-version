be89a96b8b84baabcf52cd2a8935eccedd0b229e
Ramon Paolo Maram <ramonpaolomaran12@gmail.com>
2023-02-25 22:16:23 -0300

    feat: created the action

---
+++ index.js
@@ -0,0 +1,26 @@
+const core = require('@actions/core');
+const path = require('path')
+const fs = require ('fs');
+
+try {
+  const tag = core.getInput('tag');
+  const parsedTag = tag.replace('v', '')
+
+  core.info(`Parsin tag ${tag} to ${parsedTag}!`);
+
+  const packagePath = path.join('.', 'package.json')
+
+  const packageString = fs.readFileSync(packagePath, {
+    encoding: 'utf-8'
+  })
+
+  const packageJson = JSON.parse(packageString)
+  
+  packageJson['version'] = parsedTag
+
+  fs.writeFileSync(packagePath, JSON.stringify(packageJson))
+  
+  core.setOutput('parsed-tag', parsedTag);
+} catch (error) {
+  core.setFailed(error.message);
+}
